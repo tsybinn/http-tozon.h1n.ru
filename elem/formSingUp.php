a4cd534c0c653673c107b4a17030ac2a80367fc8
tsybinn <tsyibinn@gmail.com>
2019-04-04 17:38:20 +0300

    added header

---
+++ elem/formSingUp.php
@@ -0,0 +1,141 @@
+<?php
+//require_once "../Interface/Iuser.php";
+//
+//
+// class User implements Iuser {
+//
+//public function singUp()
+//{
+//
+//}
+//
+// };
+
+$host = "localHost";
+$dbname = "gbook";
+$user = "root";
+$password = "";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+       $db->exec("set names utf8");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $db->prepare("INSERT INTO msgs (name,password,email) VALUES (:name,:password, :email)");
+
+
+    $stmt -> execute(array('name'=>'perfect', 'password'=>'ih','email'=>'cymntgh@bbt.ru'));
+
+
+
+
+
+
+
+
+
+
+//    $stmt = $db->prepare("SELECT * FROM msgs WHERE name=? AND password=?");
+//    $stmt->bindValue(1, 'valera', PDO::PARAM_STR);
+//    $stmt->bindValue(2, '2222', PDO::PARAM_INT);
+
+
+   // $stmt->bindValue(1, 'dima', PDO::PARAM_STR);
+   // $stmt->bindValue(2, '2221', PDO::PARAM_INT);
+
+
+   // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//      foreach($rows as $elem){
+//
+//          echo "$elem[name]";
+//          echo "$elem[password] .  <hr>";
+
+
+
+
+     // }
+
+   // ;
+
+
+
+
+//  $sql = "SELECT * FROM msgs WHERE name='valera' AND password=2222";
+//
+//
+//    $stmt = $db->query($sql);
+////Установка fetch mode
+//    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+//
+//    var_dump($stmt);
+//    while($row = $stmt->fetch())
+//    {
+//        echo "<p>" . $row['name'] . "&nbsp;" . $row['password'] . "</p>";
+//
+//    }
+
+  //if ($user == 0)
+
+
+
+
+
+
+               // echo $sql;
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+<form method="POST" action="">
+    <div class="form-group">
+        <label for="login"></label>
+        <input type="email" class="form-control border border-danger " name="login" aria-describedby="emailHelp"
+               placeholder="login">
+        <small name="emailHelp" class="form-text text-muted "
+        </small>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1"></label>
+        <input type="email" class="form-control" name="exampleInputEmail1" aria-describedby="emailHelp"
+               placeholder="Enter email">
+        <small name="emailHelp" class="form-text text-muted"></small>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1"></label>
+        <input type="password" class="form-control  error" name="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1"></label>
+        <input type="password" class="form-control error" name="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary">
+            to register
+        </button>
+    </div>
+
+
+</form>
+
+
+
+</body>
+</html>
+
+
+
---
+++ elem/header.php
@@ -0,0 +1,74 @@
+
+
+<div class="headerInfo">
+
+    <div class ="infoNav">
+
+        <ul class="ulInfoNav">
+            <li class="ulInfoNavLocal"> <img src="img/location.png "
+                                        width="20" height="20" alt="local">
+                <a class="linkInfoNav" href="#">Москва</a></li>
+
+                <li class="ulInfoNav"><a class="linkInfoNav" href="#">
+
+                 <p>Пункты Выдычи заказов</p></a> </li>
+
+             <li class="ulInfoNav"><a class="linkInfoNav" href="#">
+
+                     <p>Условие доставки</p> </a></li>
+
+            <li class="ulInfoNav"><a class="linkInfoNav" href="#">
+
+                    <p>  Условие оплаты</p></a> </li>
+
+            <li class="ulInfoNav"><a class="linkInfoNav" href="#">
+
+                    <p>Условие Возврата</p></a></li>
+
+            <li class="ulInfoNav"><a class="linkInfoNav" href="#">
+
+                    <p> Помощь</p></a>
+            </li>  <li class="ulInfoNavCon"><img src="img/24.png" width="25" height="25"><p>+ 7-000-000-00-00</p>
+         </li>
+
+
+
+        </ul>
+
+
+    </div>
+
+</div>
+
+<div class="headerSearch">
+    <div class="label "><a href="#">TOZON</a>
+    </div>
+
+    <div class="search ">
+        <input class="searchString" type="search" name="q" placeholder="Поиск по сайту">
+        <input  type="submit" class="searchSubmit" name="send" value="Отправить" >
+    </div>
+
+    <div class="headerLogin ">
+        <li class=""><a href="#"><img src="img/profle.png" width="40"  height="40" alt="login"><p>sing</p> </a>    </li>
+        <li class=""><a href="#"><img src="img/frames.png" width="40"  height="40" alt="login"><p>orders</p> </a>  </li>
+        <li class=""><a href="#"><img src="img/heart.png" width="40"  height="40" alt="login"><p>favorites</p> </a> </li>
+        <li class=""><a href="#"><img src="img/empty.png" width="40"  height="40" alt="login"><p>basket</p> </a>   </li>
+
+    </div>
+</div>
+
+<div class="headerCategory">
+
+    <ul class="ulHeaderCategory">
+        <li><a class="linkHeadCategory border" href="#">Все разделы</a></li>
+        <li><a class="linkHeadCategory border" href="#">Все все акции</a></li>
+        <li><a class="linkHeadCategory border" href="#">Все  магазины</a></li>
+        <li><a class="linkHeadCategory border" href="#">Электроника</a></li>
+        <li><a class="linkHeadCategory border" href="#">Бытовая техника</a></li>
+
+
+
+    </ul></div>
+
+
